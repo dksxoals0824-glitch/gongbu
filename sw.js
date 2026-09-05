7b437a5fe4b5a886531ed96dcca01535102170b9
dksxoals0824-glitch <dksxoals0824@gmail.com>
2026-09-05 16:01:23 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'gongbu-v2';
+const CACHE = 'gongbu-v3';
 const ASSETS = ['./', './index.html', './manifest.webmanifest',
   './icon-192.png', './icon-512.png', './apple-touch-icon.png'];
 

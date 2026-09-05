ebefd49ded88af5e400d7b5097ce67b52d4d9ccc
dksxoals0824-glitch <dksxoals0824@gmail.com>
2026-09-05 14:46:20 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE = 'gongbu-v1';
+const ASSETS = ['./', './index.html', './manifest.webmanifest',
+  './icon-192.png', './icon-512.png', './apple-touch-icon.png'];
+
+self.addEventListener('install', e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));
+});
+self.addEventListener('activate', e=>{
+  e.waitUntil(caches.keys().then(ks=>Promise.all(
+    ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));
+});
+// 항상 새 버전을 먼저 확인하고, 인터넷이 없으면 저장해둔 것을 쓴다
+self.addEventListener('fetch', e=>{
+  if(e.request.method !== 'GET') return;
+  e.respondWith(
+    fetch(e.request).then(res=>{
+      const copy = res.clone();
+      caches.open(CACHE).then(c=>c.put(e.request, copy)).catch(()=>{});
+      return res;
+    }).catch(()=> caches.match(e.request).then(r=> r || caches.match('./index.html')))
+  );
+});
